a48b9b325eaa45d1178af3fa0ce0b3c96b43734d
zeroOone <271740784@qq.com>
2018-05-22 15:39:28 +0800

    2018-05-22 pom.xml添加spring框架依赖的必需库；新建了一个spring实例（HelloSpring）

---
+++ src/main/java/spring/firstSpring/HelloSpring.java
@@ -0,0 +1,13 @@
+package spring.firstSpring;
+
+public class HelloSpring {
+    private String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void getMessage() {
+        System.out.println("Your message : " + message);
+    }
+}
