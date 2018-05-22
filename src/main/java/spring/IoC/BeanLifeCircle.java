43cb8d6d9e6dc7454c4f3b31ea42ea60e2ba929f
zeroOone <271740784@qq.com>
2018-05-22 17:46:03 +0800

    2018-05-22 代码优化

--- src/main/java/spring/IoC/BeanLifeCircle.java
+++ src/main/java/spring/IoC/BeanLifeCircle.java
@@ -19,9 +19,9 @@ public class BeanLifeCircle{
         //客制化出示与销毁方法
         HelloSpring obj2 = (HelloSpring) context.getBean("lifeCircleCreated");
         obj2.getMessage();
-        ((ClassPathXmlApplicationContext) context).registerShutdownHook();
+       // ((ClassPathXmlApplicationContext) context).registerShutdownHook();
         /**
-         * 通过这个例子，还可以看出，beans对象是在容器中统一初始化的，当被需要的时候，直接分配，
+         * 通过这个例子，还可以看出，beans对象是在容器中统一初始化的，当bean对象被需要的时候，直接分配，
          * 销毁的时候，也是一并销毁。
          * 不存在只初始化或者销毁其中一个bean的用法；
          * 这里突出了BeanFactory的一个优点，就是对bean对象的统一管理
